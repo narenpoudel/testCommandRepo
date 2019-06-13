f3043cb948b4741c59769ffe28f5af04140b6c0c
narenpoudel <35817725+narenpoudel@users.noreply.github.com>
2019-06-12 18:24:20 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Apline Info</title>
+	  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+	
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+		<div class="container">
+			<a class="navbar-brand" href="#">APLINE INFO</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse justify-content-end" id="navbarText">
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+					</li>
+
+
+						<li class="nav-item dropdown">
+					        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					         About us
+					        </a>
+					        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+					          <a class="dropdown-item" href="#about">Who we are</a>
+					          <a class="dropdown-item" href="#ceo">Message from CEO</a>
+					        </div>
+					      </li>
+
+					<li class="nav-item">
+						<a class="nav-link" href="#abroad">Study Abroad</a>
+					</li>
+			
+					<li class="nav-item">
+						<a class="nav-link" href="#contact">Contact us</a>
+					</li>
+				</ul>
+
+			</div>
+		</div>
+	</nav>
+
+	<div id="stage">
+		<div id="stage-caption">
+			<h1 class="display-3">An award-winning consultant for international students takes good care of you</h1>
+			<a href="#" class="btn btn-lg btn-success">Contact us today!</a>
+		</div>
+		
+	</div>
+	<section id="about">
+		<div class="container">
+			<div class="row about_us">
+				<h1>ABOUT US</h1>
+				<p class="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+				<p class="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+				<p class="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+			</div>
+		</div>
+	</section>
+
+		<section id="ceo">
+		<div class="container">
+			<h1 class="message">Message from CEO</h1>
+			<div class="row">
+				
+				<div class="col-lg-7 ">
+					<p class="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+					<p class="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+				</div>
+				<div class="col-lg-4 offset-lg-1">
+					<img src="img/ceo.jpg" alt="CEO" />
+					<h2>Chief Executive Officer</h2>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="abroad">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-5"><img src="img/group.jpg" alt="CEO" /></div>
+				<div class="col-lg-6 offset-lg-1">
+					<h2>Why study in USA?</h2>
+					<p class="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+					<a href="#" class="btn btn-lg btn-success">Contact us today!</a>
+
+				</div>	
+			</div>
+			<div class="row edu_sys">
+					<div class="col-lg-5">
+					<h2>American Education System</h2>
+					<p class="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+				</div>
+				<div class="col-lg-6 offset-lg-1"><img src="img/sys.jpg" alt="Education System" /></div>
+			</div>
+
+		</div>
+	</section>
+	
+
+	<section id="contact">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<form>
+						<div class="form-group">
+							<label for="name">Name</label>
+							<input type="text" class="form-control" id="name" aria-describedby="emailHelp" placeholder="Enter Name">
+						</div>
+						<div class="form-group">
+							<label for="email">Email address</label>
+							<input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+							<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+						</div>
+						<div class="form-group">
+							<label for="message">Message</label>
+							<textarea class="form-control" id="message" rows="3"></textarea>
+						</div>
+						<button type="submit" class="btn btn-success">Submit</button>
+					</form>
+				</div>
+				<div class="col-lg-5 offset-lg-1">
+					<div class="row">
+						<div class="col-lg-12">
+							<h2>Address</h2>
+							<p>350 W 114 Avenue, Suit 1608</p>
+							<p>Northglenn, Colorado 80234</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-1">
+							<a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook-square" style="font-size:2rem;color:#fff"></i></a>
+						</div>
+						<div class="col-sm-1">
+							<a href="https://www.linkedin.com/" target="_blank"><i class="fa fa-linkedin-square" style="font-size:2rem;color:#fff"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<footer class="main-footer">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-6">
+					Copyright @ 2019 Apline Info LLC. All right reserved
+				</div>
+				<div class="col-md-2 offset-lg-4 justify-content-end">
+					Designed by <a href="http://aplineinfo.com" target="_blank">Apline Info</a>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
